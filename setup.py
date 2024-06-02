26ed1a8c2e5b424dfc07563e7f9ec98689616223
RogerQi <rizhaoq2@illinois.edu>
2024-06-02 04:13:42 -0700

    [FIX] make TIMM mandatory dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="mobilesamv2",
     version="1.0",
-    install_requires=[],
+    install_requires=['timm'],
     packages=find_packages(exclude="notebooks"),
     extras_require={
         "all": ["matplotlib", "pycocotools", "opencv-python", "onnx", "onnxruntime"],
