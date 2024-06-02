ddceb87f6b9d58d1e73c5eff27bc191d571ea6a0
RogerQi <rizhaoq2@illinois.edu>
2024-06-02 13:58:39 -0700

    [FIX] add YOLO cfg to package data for installation w/o -e

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name="mobilesamv2",
     version="1.0",
-    install_requires=['timm'],
+    include_package_data=True,
+    package_data={'ultralytics': ['yolo/cfg/default.yaml']},
+    install_requires=["timm", "torchpack", "onnx", "onnxruntime", "onnxsim"],
     packages=find_packages(exclude="notebooks"),
     extras_require={
-        "all": ["matplotlib", "pycocotools", "opencv-python", "onnx", "onnxruntime"],
+        "all": ["matplotlib", "pycocotools", "opencv-python", "", "onnxruntime"],
         "dev": ["flake8", "isort", "black", "mypy"],
     },
 )
